22da994b58db1e2f7c1be00897ab2c2d56be21d7
Damian Bradicich <dbradicich@sonatype.com>
2021-10-15 16:05:46 -0400

    NEXUS-29025: prevent reflective poll error response (#20)

--- directjngine/src/main/java/com/softwarementors/extjs/djn/router/processor/ServerExceptionInformation.java
+++ directjngine/src/main/java/com/softwarementors/extjs/djn/router/processor/ServerExceptionInformation.java
@@ -8,7 +8,7 @@ import com.softwarementors.extjs.djn.ExceptionUtils;
 /* Information about unexpected server exception, passed
  * to the Javascript side
  */
-/* package */ class ServerExceptionInformation {
+public class ServerExceptionInformation {
    
   /* package */ ExceptionInformation rootException;
   /* package */ ExceptionInformation exception;
@@ -25,13 +25,13 @@ import com.softwarementors.extjs.djn.ExceptionUtils;
     this.rootException = this.exceptions.get(this.exceptions.size()-1);
   }
   
-  /* package */ static class ExceptionInformation {
+  public static class ExceptionInformation {
     @edu.umd.cs.findbugs.annotations.SuppressWarnings( value="URF_UNREAD_FIELD", justification="Passed to JSON side only")
-    /* package */ String type;
+    public String type;
     @edu.umd.cs.findbugs.annotations.SuppressWarnings( value="URF_UNREAD_FIELD", justification="Passed to JSON side only")
-    /* package */ String message;
+    public String message;
     @edu.umd.cs.findbugs.annotations.SuppressWarnings( value="URF_UNREAD_FIELD", justification="Passed to JSON side only")
-    /* package */ String where;
+    public String where;
     // Future development: we may have an exception handler that allows
     // us to register exception processors to pass extra data
     // private Map<String, Object> extraData = new HashMap<String,Object>();
